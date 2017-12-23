90606ac9f93f6188f68063a807bf389da8133ca5
Stephane <son.trinh.dev@gmail.com>
2017-12-23 14:26:34 +0000

    automatically backed up by learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -6,5 +6,6 @@ def turn_count(board)
   sum
 end
 def current_player(board, turn_count)
-  turn_count(board).even? ? 'X' : 'O'
-end
+  turn_count = turn_count(board)
+  turncount.even? ? 'X' : 'O'
+end 
