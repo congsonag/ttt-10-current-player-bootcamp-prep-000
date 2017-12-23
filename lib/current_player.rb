92f1b3c2800dc5b7655a6aa0298bb84db7881580
Stephane <son.trinh.dev@gmail.com>
2017-12-23 14:33:13 +0000

    automatically backed up by learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -1,11 +1,12 @@
 def turn_count(board)
   sum = 0
-  board.each do |turn|
-    sum += 1 if turn == 'X' || turn == 'O'
+  board.each do |element|
+    if element == 'X' || element == 'O'
+      sum += 1 
+    end
   end
-  sum
 end
-def current_player(board, turn_count)
-  turn_count = turn_count(board)
-  turncount.even? ? 'X' : 'O'
-end 
+
+def current_player(board)
+  turn_count(board).even? ? 'X' : 'O'
+end
